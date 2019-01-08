8103c8a4ab0379b8bdb110e14dc880c2a4d262fa
Melojan Rangithathas <melojan@hotmail.de>
2019-01-08 10:48:35 +0000

    Modified

--- SQL/CodeSignal/SQL Practices.sql
+++ SQL/CodeSignal/SQL Practices.sql
@@ -85,3 +85,58 @@ WHERE NOT
 (Suspect.height > 170 AND lower(Suspect.surname) LIKE 'Gre_n' AND lower(Suspect.name) LIKE 'b%')
 ORDER BY id;
 END
+
+securityBreach :
+
+CREATE PROCEDURE securityBreach()
+BEGIN
+SELECT * FROM users WHERE attribute LIKE BINARY CONCAT('%_\%,first_name, '\_', second_name, '\%%') ORDER BY attribute ASC;
+END
+
+testCheck :
+
+CREATE PROCEDURE testCheck()
+SELECT id, IF (given_answer IS NULL , 'no answer' , IF(given_answer = correct_answer, 'correct','incorrect')) AS checks
+FROM answers
+ORDER BY id;
+
+expressionsVerfication :
+
+CREATE PROCEDURE expressionsVerification()
+BEGIN
+SELECT * FROM expressions
+WHERE
+CASE operation
+WHEN '+' THEN a + b  
+WHEN '-' THEN a - b
+WHEN '/' THEN a / b
+WHEN '*' THEN a * b
+END = c
+ORDER BY id ASC;
+END
+
+newsSubscribers :
+
+CREATE PROCEDURE newsSubscribers()
+BEGIN
+SELECT subscriber FROM full_year WHERE newspaper LIKE '%DAILY%'
+UNION
+SELECT subscriber FROM half_year WHERE newspaper LIKE '%DAILY%'
+ORDER BY subscriber ASC;
+END
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
