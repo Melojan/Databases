28737f6e52231de1e1c8d2dd050db086810bfd05
Melojan Rangithathas <melojan@hotmail.de>
2019-01-09 20:24:14 +0000

    Editing Files

--- SQL/CodeSignal/SQL Practices.sql
+++ SQL/CodeSignal/SQL Practices.sql
@@ -189,6 +189,42 @@ UNION
 SELECT 'Total:',COUNT(country) AS competitors FROM foreignCompetitors;
 END
 
+personalHobbies :
+
+CREATE PROCEDURE personalHobbies()
+BEGIN
+SELECT name FROM people_hobbies WHERE FIND_IN_SET('reading',hobbies) AND FIND_IN_SET('sports'hobbies);
+END
+
+orderOfSuccession :
+
+CREATE PROCEDURE orderOfSuccession()
+BEGIN
+SELECT
+(CASE 
+WHEN gender = 'F' THEN CONCAT('Queen ',name)
+WHEN gender = 'M' THEN CONCAT('King ',name)
+END) as name
+FROM Successors
+ORDER BY birthday;
+END
+
+placesOfInterest :
+
+CREATE PROCEDURE placesOfInterest()
+BEGIN
+SELECT r.country, sum(r.adventure_park) as adventure_park, sum(r.golf) as golf, sum(r.river_cruise) as river_cruise, sum(r.kart_racing) as kart_racing 
+ FROM ( 
+     SELECT country, 
+     CASE WHEN leisure_activity_type = 'Adventure park' THEN number_of_places ELSE 0 END as adventure_park, 
+     CASE WHEN leisure_activity_type = 'Golf' THEN  number_of_places ELSE 0 END as golf, 
+     CASE WHEN leisure_activity_type = 'River cruise' THEN number_of_places ELSE 0 END as river_cruise, 
+     CASE WHEN leisure_activity_type = 'Kart racing' THEN number_of_places ELSE 0 END as kart_racing 
+     FROM countryActivities 
+     ) r 
+ GROUP by r.country; 	
+END
+
 
 
 
